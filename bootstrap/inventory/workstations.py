f6a726508cdf9f4a4f8ab5f11ab640fa5e8893c0
Rob Verduijn <rob.verduijn@gmail.com>
2021-06-21 21:54:36 +0200

    created dynamic inventory for workstations

--- bootstrap/inventory/workstations.py
+++ bootstrap/inventory/workstations.py
@@ -1,81 +1,76 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-# (c) 2019 Jacob Salmela <me@jacobsalmela.com>
-# GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
-
-ANSIBLE_METADATA = {'metadata_version': '1.0',
-                    'status': ['preview'],
-                    'supported_by': 'community'}
-
-DOCUMENTATION = """
-short_description: A dynamic inventory script that uses the Spacewalk API
-description:
-    - A dynamic inventory script for Ansible
-version_added: "2.8"
-options:
-# One or more of the following
-    option_name:
-        description:
-            - Description of the options goes here.
-            - Must be written in sentences.
-        required: true or false
-        default: a string or the word null
-        choices:
-          - enable
-          - disable
-        aliases:
-          - repo_name
-        version_added: "2.8"
-requirements:
-    - Click == 7.0
-"""
-
-# This file is part of Ansible.
-#
-# Ansible is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# Ansible is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
-#
-
-# notes:
-# Tested on Ansible v2.9.21
-# This is a dynamic inventory script 
-
-import json
-import sys
 import os
+import sys
 import argparse
 
+try:
+    import json
+except ImportError:
+    import simplejson as json
+
 number_of_workstations = os.getenv('WORKSTATIONS', '2')
-domain = os.getenv('DOMAIN', '2')
+domain = os.getenv('DOMAIN', 'qstars.lab')
 
-# dynamic_inventory = { "group": { "hosts": [], "vars": {} }, "_meta": {} } }
-dynamic_inventory = {}
-dynamic_inventory["workstations"] = { "hosts": [], "vars": {}, "_meta": {} }
+class ExampleInventory(object):
 
+    def __init__(self):
+        self.inventory = {}
+        self.read_cli_args()
 
-def gen_inventory():
-    """Generate hosts and groups and vars"""
-    for workstation in range(number_of_workstations + 10):
-      dynamic_inventory["workstations"]["hosts"].append('workstation' + workstation + '.' + domain)
-    inventory = json.dumps(dynamic_inventory)
-    print(inventory)
+        # Called with `--list`.
+        if self.args.list:
+            self.inventory = self.example_inventory()
+        # Called with `--host [hostname]`.
+        elif self.args.host:
+            # Not implemented, since we return _meta info `--list`.
+            self.inventory = self.empty_inventory()
+        # If no groups or vars are present, return an empty inventory.
+        else:
+            self.inventory = self.empty_inventory()
 
-def workstation_inventory():
-    """A dynamic inventory script for Ansible"""
-    pass
+        print(json.dumps(self.inventory))
 
+    # Example inventory for testing.
+    def example_inventory(self):
+        inventory={
+            'workstations': { 'hosts': [], 'vars': {} },
+            'ipaclients': { 'hosts': [], 'vars': {} },
+            '_meta': { 'hostvars': {} }}
+        for host in range(1,int(number_of_workstations)+1):
+            workstation='workstation' + str(host + 10) + '.' + domain
+            ipaddress='10.10.1.' + str(host + 10)
+            for group in ['ipaclients', 'workstations']:
+                inventory[group]['hosts'].append(workstation)
+            inventory['_meta']['hostvars'][workstation]={
+                    'ansible_host': ipaddress,
+                    'ansible_user': 'student',
+                    'network_connections':
+                    [
+                        { 
+                        'name': 'eth0', 'state': 'up', 'type': 'ethernet',
+                        'interface_name': 'eth0', 'ip': {
+                            'address': [ ipaddress + '/24'],
+                            'dns': ['10.10.1.10'], 'dns_search': [domain], 'gateway4': '10.10.1.1'
+                            },
+                        },
+                        { 'name': 'ens3', 'state': 'absent' },
+                        { 'name': 'System eth0', 'state': 'absent' }
+                    ]
+                }
+        return inventory
 
-if __name__ == '__main__':
-    workstation_inventory()
+    # Empty inventory for testing.
+    def empty_inventory(self):
+        return {'_meta': {'hostvars': {}}}
+
+    # Read the command line args passed to the script.
+    def read_cli_args(self):
+        parser = argparse.ArgumentParser()
+        parser.add_argument('--list', action = 'store_true')
+        parser.add_argument('--host', action = 'store')
+        self.args = parser.parse_args()
+
+# Get the inventory.
+ExampleInventory()
+
     
